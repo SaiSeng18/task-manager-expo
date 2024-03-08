d27b85bd8f6a97c9aad67cddf81bbaa1b2da1ec2
SaiSeng18 <saisengmongli14@gmail.com>
2024-03-08 20:46:51 +0630

    chore: create home page

---
+++ app/index.tsx
@@ -0,0 +1,40 @@
+import { Link } from "expo-router";
+import { ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { COLORS, SPACING } from "@/constants/theme";
+import { Header } from "@/components/home-page/Header";
+import { CurrentTask } from "@/components/home-page/CurrentTask";
+import { PreviewTask } from "@/components/home-page/PreviewTask";
+import { CommunityBlog } from "@/components/home-page/CommunityBlog";
+
+export default function Page() {
+	return (
+		<ScrollView
+			style={{
+				flex: 1,
+				marginTop: StatusBar.currentHeight,
+				backgroundColor: COLORS.dark,
+			}}>
+			<View style={styles.container}>
+				<Header />
+
+				<CurrentTask />
+
+				<PreviewTask />
+
+				<CommunityBlog />
+
+				<Link href="/tasks">Tasks</Link>
+			</View>
+		</ScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		height: "100%",
+		alignItems: "center",
+		padding: SPACING.small,
+		gap: SPACING.small,
+	},
+});
